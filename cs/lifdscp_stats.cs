b493f9eada5fde9c6adddefcab9f3728e0e0a158
Michael Khaimov <khaimovmr@gmail.com>
2015-11-29 02:35:57 +0200

    Finishing refactoring.

--- cs/lifdscp_stats.cs
+++ cs/lifdscp_stats.cs
@@ -2,66 +2,64 @@ cancel($LIFDS_UPDATE_TIMER);
 
 $LIFDS_ONLINE_UPDATE_INTERVAL = 10000; //Update list every 10 seconds
 
-$LIFDS_nullObj = new SimObject();
+$LIFDSCP_nullObj = new SimObject();
 
-package LiFDSCP
+package LIFDSCP
 {
-
-function LIFDS_sqlNullCallback(%rs) 
-{
-    dbi.remove(%rs);
-    %rs.delete();
-}
-
-function LIFDS_sqlExecute(%sql)
-{
-    echo("QUERY: " @ %sql);
-    dbi.Execute(%sql, LIFDS_sqlNullCallback, $LIFDS_nullObj);
-}
-
-function LIFDS_onTimerElapsed()
-{
-    echo("Updating" SPC ClientGroup.getCount() SPC "players...");
-    %onlineChars = "";
-    %haveChars = 0;
-    sqlExecute("begin");
-
-    for(%id = 0; %id < ClientGroup.getCount(); %id++)
+    function LIFDSCP_sqlNullCallback(%rs) 
     {
-        %client = ClientGroup.getObject(%id);
-        %pid = %client.getCharacterId();
-        LIFDS_updatePlayer(%pid,%client);
+        dbi.remove(%rs);
+        %rs.delete();
+    }
 
-        if (%id > 0)
+    function LIFDSCP_sqlExecute(%sql)
+    {
+        echo("QUERY: " @ %sql);
+        dbi.Execute(%sql, LIFDSCP_sqlNullCallback, $LIFDSCP_nullObj);
+    }
+
+    function LIFDSCP_onTimerElapsed()
+    {
+        echo("Updating" SPC ClientGroup.getCount() SPC "players...");
+        %onlineChars = "";
+        %haveChars = 0;
+        LIFDSCP_sqlExecute("begin");
+
+        for(%id = 0; %id < ClientGroup.getCount(); %id++)
         {
-            %onlineChars = %onlineChars @ ", ";
+            %client = ClientGroup.getObject(%id);
+            %pid = %client.getCharacterId();
+            LIFDSCP_updatePlayer(%pid, %client);
+
+            if (%id > 0)
+            {
+                %onlineChars = %onlineChars @ ", ";
+            }
+            
+            %onlineChars = %onlineChars @ %pid;
+            %haveChars = 1;
         }
-		
-        %onlineChars = %onlineChars @ %pid;
-        %haveChars = 1;
+
+        if (%haveChars > 0)
+        {
+            LIFDSCP_sqlExecute("delete from lifdscp_online_character where CharacterID not in (" @ %onlineChars @ ")");
+        }
+        else
+        {
+            LIFDSCP_sqlExecute("delete from lifdscp_online_character");
+        }
+
+        LIFDSCP_sqlExecute("commit");
+        $LIFDS_UPDATE_TIMER = schedule($LIFDS_ONLINE_UPDATE_INTERVAL, 0, "LIFDSCP_onTimerElapsed");
     }
 
-    if (%haveChars > 0)
+    function LIFDSCP_updatePlayer(%pid, %obj)
     {
-        LIFDS_sqlExecute("delete from lifdscp_online_character where CharacterID not in (" @ %onlineChars @ ")");
+        echo("Updating player " @ %pid);
+        LIFDSCP_sqlExecute("insert ignore lifdscp_online_character (CharacterID) VALUES (" @ %pid @ ")");
     }
-    else
-    {
-        LIFDS_sqlExecute("delete from lifdscp_online_character");
-    }
-
-    LIFDS_sqlExecute("commit");
-    $LIFDS_UPDATE_TIMER = schedule($LIFDS_ONLINE_UPDATE_INTERVAL, 0, "LIFDS_onTimerElapsed");
-}
-
-function LIFDS_updatePlayer(%pid, %obj)
-{
-    echo("Updating player " @ %pid);
-    LIFDS_sqlExecute("insert ignore lifdscp_online_character (CharacterID) VALUES (" @ %pid @ ")");
-}
-
 };
 
-ActivatePackage(LiFDSCP);
+ActivatePackage(LIFDSCP);
 
-$LIFDS_UPDATE_TIMER = schedule(100, 0, "LIFDS_onTimerElapsed");
+$LIFDS_UPDATE_TIMER = schedule(100, 0, "LIFDSCP_onTimerElapsed");
